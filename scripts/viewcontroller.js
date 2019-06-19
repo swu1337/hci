fab7a263c431f1defbe33e0b5d4d77a52cf97745
swu1337 <master_msp1@hotmail.com>
2019-06-19 15:47:25 +0200

    first day half

--- scripts/viewcontroller.js
+++ scripts/viewcontroller.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	//Hack to add required html classes in order for the library to work
 	$('main').addClass('mainbag').children().addClass('mainview');
 	$('.mainbag').viewScroller({
-		animSpeedMainView: 1800, // Animation speed of mainviews: 0 - fastest
+		animSpeedMainView: 600, // Animation speed of mainviews: 0 - fastest
 		useScrollbar: false, // Use scrollbar to change views
 		changeWhenAnim: false, // Change views when they are changing
 		loopMainViews: true, // Change horizontal views (mainviews) in loop mode
